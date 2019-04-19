8380e003409cbc28034be8d257ce18dac9782e1a
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-04-19 18:18:47 +0200

    [EUT-59] (feat): add database setup script

---
+++ build/dbSetup.sql
@@ -0,0 +1,14 @@
+CREATE SCHEMA IF NOT EXISTS sc_user;
+
+DROP TABLE IF EXISTS sc_user;
+CREATE TABLE sc_user.user (
+  uid       char(20)      PRIMARY KEY,
+  email     varchar(70)   UNIQUE NOT NULL,
+  name      varchar(70),
+  callname  varchar(70)
+);
+
+DROP USER IF EXISTS service_user;
+CREATE USER service_user;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA sc_user TO service_user;
